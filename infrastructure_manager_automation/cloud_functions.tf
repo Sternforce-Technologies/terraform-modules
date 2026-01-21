b6be89c3d6a87bae44fb6ff40d8c3dbd83e8bcfd
Joshua Barry <josh@sternforce.com>
2026-01-20 21:45:07 -0500

    change reference to repo

--- infrastructure_manager_automation/cloud_functions.tf
+++ infrastructure_manager_automation/cloud_functions.tf
@@ -12,9 +12,9 @@ resource "google_cloudfunctions2_function" "auditor_function" {
     source {
       repo_source {
         project_id   = var.project_id
-        repo_name    = google_cloudbuildv2_repository.github_module_repo.name
+        repo_name    = google_cloudbuildv2_repository.github_module_repo.id
         branch_name  = "main"
-        dir        = "infrastructure_manager_automation/im-audit"
+        dir          = "infrastructure_manager_automation/im-audit"
       }
     }
   }
